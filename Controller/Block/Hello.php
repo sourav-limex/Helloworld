7b31ace0f864a24f32fc035bce8e55413122446c
sourav rout <82997088+sourav-limex@users.noreply.github.com>
2021-05-14 12:53:55 +0530

    Update Hello.php

--- Controller/Block/Hello.php
+++ Controller/Block/Hello.php
@@ -1,12 +1,4 @@
 <?php
-/**
- *
- * @package     magento2
- * @author      Sourav Technologies
- * @license     https://opensource.org/licenses/OSL-3.0 Open Software License v. 3.0 (OSL-3.0)
-
- */
-
 namespace Sourav\HelloWorld\Block;
 
 use Magento\Framework\View\Element\Template;
